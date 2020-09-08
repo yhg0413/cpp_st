b189c8a3478d3da5c1c7c30894bbb363a8066095
yang <yanghuigeon9@gmail.com>
2020-09-08 17:09:34 +0900

    2020-09-08 save

--- chapter03/ex02_call_by_value.cpp
+++ chapter03/ex02_call_by_value.cpp
@@ -12,7 +12,7 @@ int main(int argc, char const *argv[]){
     int a= 100, b = 200;
 
     printf("a=%d, b=%d\n", a, b);
-    swap(a,b);
+    swap(a,b);// 지역변수 x와 y의 값만 바뀌기 때문에 main의 a와 b는 바뀌지않음
     printf("a=%d, b=%d\n", a, b);
     
     return 0;
